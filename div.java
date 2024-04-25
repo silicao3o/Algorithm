66da76140659fb26cfbae56be2c0efc4733497d3
silicao3o <zergskybmw@gmail.com>
2024-04-25 11:28:55 +0900

    카타

---
+++ div.java
@@ -0,0 +1,6 @@
+public class div {
+    public int solution(int num1, int num2) {
+
+        return num1 / num2;
+    }
+}
---
+++ minus.java
@@ -0,0 +1,6 @@
+public class minus {
+    public int solution(int num1, int num2) {
+
+        return num1 - num2;
+    }
+}
