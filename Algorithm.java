4cf87d7e6c8785a4d24e7859fcf15a0757a0b243
silicao3o <zergskybmw@gmail.com>
2024-04-26 09:43:02 +0900

    두수 비교

--- Algorithm.java
+++ Algorithm.java
@@ -1,6 +1,13 @@
 public class Algorithm {
-    public int solution(int age) {
+    public int solution(int num1, int num2) {
+        if(num1 == num2){
+            int answer = 1;
+            return answer;
+        }
+        else {
+            int answer = -1;
+            return answer;
+        }
 
-        return 2022 - age + 1;
     }
 }
